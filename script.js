e89f91f9f93b00d3ac3dae4a1b030b1546c2bb2a
Velvet <172956088+tsukinoinaba@users.noreply.github.com>
2024-08-24 15:56:42 +0800

    Update display when buttons are pressed

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let a = 0;
 let b = 0;
 let op = "";
+const display = document.querySelector("#display");
+const numBtns = document.querySelectorAll(".numBtn");
+const opBtns = document.querySelectorAll(".opBtn");
+const operators = ["+", "-", "*", "/"];
 
 function add (a, b) {
     return a + b;
@@ -15,6 +19,9 @@ function multiply (a, b) {
 }
 
 function divide (a, b) {
+    if (b === 0) {
+        return "Do you play Shippu Mahou Daisakusen too?";
+    }
     return a / b;
 }
 
@@ -30,3 +37,24 @@ function operate (a, op, b) {
             return divide(a, b);
     }
 }
+
+function updateDisplay() {
+    const c = this.textContent;
+
+    if (display.textContent === "0" && !operators.includes(c)) {
+        display.textContent = c;
+        return;
+    }
+
+    display.textContent += c;
+}
+
+for (let btn of numBtns) {
+    btn.addEventListener("click", updateDisplay);
+}
+
+for (let btn of opBtns) {
+    btn.addEventListener("click", updateDisplay);
+}
+
+display.textContent = "0";
